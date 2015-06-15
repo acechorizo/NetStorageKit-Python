c459259d0015227b6013c899df993df3ec4f98f4
Ernesto Mendoza Blanco <ernestom@menta.com.mx>
2015-06-15 18:20:33 -0500

    Update tests with mandatory host parameter.

--- tests.py
+++ tests.py
@@ -31,7 +31,7 @@ def test_http_headers():
 
     # Test auth function calls inside Request
 
-    request = ns.api.Request(key_name, key, cpcode,
+    request = ns.api.Request(key_name, key, cpcode, 'host',
                              timestamp='1280000000', unique_id='382644692',
                              testing=True)
     parameters = {'md5': '0123456789abcdef0123456789abcdef', 'mtime': '1260000000'}
@@ -50,25 +50,25 @@ def test_http_responses():
         return callback
 
     # Secure Hostname
-    request = ns.api.Request('test-key', '123', '12345', secure=True, testing=True)
+    request = ns.api.Request('test-key', '123', '12345', 'host', secure=True, testing=True)
     _, response = request.mock()
-    assert response.url == 'https://test-key-nsu.akamaihd.net/12345/mock'
+    assert response.url == 'https://host-nsu.akamaihd.net/12345/mock'
 
     # Insecure Hostname
-    request = ns.api.Request('test-key', '123', '12345', secure=False, testing=True)
+    request = ns.api.Request('test-key', '123', '12345', 'host', secure=False, testing=True)
     _, response = request.mock()
-    assert response.url == 'http://test-key-nsu.akamaihd.net/12345/mock'
+    assert response.url == 'http://host-nsu.akamaihd.net/12345/mock'
 
     # Valid request
     mock_response = {'status': 200, 'content_type': 'text/html'}
-    request = ns.api.Request('test-key', '123', '12345', testing=mock_response)
+    request = ns.api.Request('test-key', '123', '12345', 'host', testing=mock_response)
     _, response = request.mock()
     assert response.status_code == 200
     assert response.headers['Content-Type'] == 'text/html'
 
     # Invalid request
     mock_response = {'status': 404, 'body': 'Not Found'}
-    request = ns.api.Request('test-key', '123', '12345', testing=mock_response)
+    request = ns.api.Request('test-key', '123', '12345', 'host', testing=mock_response)
     _, response = request.mock()
     assert response.status_code == 404
     assert 'Not Found' in str(response.text)
@@ -82,7 +82,7 @@ def test_api_du():
     </du>
     """
     mock_response = {'status': 200, 'body': valid_mock_response_body}
-    request = ns.api.Request('test-key', '123', '12345', testing=mock_response)
+    request = ns.api.Request('test-key', '123', '12345', 'host', testing=mock_response)
     data, response = request.du('/du/foo')
     assert response.status_code == 200
     assert data['files'] == '12399999'
@@ -96,20 +96,20 @@ def test_api_du():
     """
     mock_response = {'status': 200, 'body': invalid_mock_response_body}
     with pytest.raises(ns.exceptions.NetStorageKitError):
-        request = ns.api.Request('test-key', '123', '12345', testing=mock_response)
+        request = ns.api.Request('test-key', '123', '12345', 'host', testing=mock_response)
         data, response = request.du('/du/foo')
     assert response.status_code == 200
 
     # Unexpected response
     mock_response = {'status': 200, 'body': '<totally-invalid-xml>'}
     with pytest.raises(ns.exceptions.NetStorageKitError) as e:
-        request = ns.api.Request('test-key', '123', '12345', testing=mock_response)
+        request = ns.api.Request('test-key', '123', '12345', 'host', testing=mock_response)
         _ = request.du('/')
     assert 'ParseError' in str(e.value)
 
     # Incomplete response
     mock_response = {'status': 200, 'body': '<du/>'}
     with pytest.raises(ns.exceptions.NetStorageKitError) as e:
-        request = ns.api.Request('test-key', '123', '12345', testing=mock_response)
+        request = ns.api.Request('test-key', '123', '12345', 'host', testing=mock_response)
         _ = request.du('/')
     assert 'AttributeError' in str(e.value)
