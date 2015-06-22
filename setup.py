f352a96fa3edffe254a9db542b07025832cf8b4b
Ernesto Mendoza Blanco <ernestom@menta.com.mx>
2015-06-22 17:23:29 -0500

    Version bump.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='NetStorageKit',
-    version='1.0.0',
+    version='1.0.1',
     description='Akamai\'s NetStorage API communication kit',
     author='Ernesto Mendoza Blanco',
     author_email='ernestom@mentanetwork.com',
