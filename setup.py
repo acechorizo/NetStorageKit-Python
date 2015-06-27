1722fa21b8259eaa7ec0b654c7ebc769abaf90a4
Ernesto Mendoza Blanco <ernestom@menta.com.mx>
2015-06-26 20:39:32 -0500

    Version 1.0.6: quick-delete.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='NetStorageKit',
-    version='1.0.5',
+    version='1.0.6',
     description='Akamai\'s NetStorage API communication kit',
     author='Ernesto Mendoza Blanco',
     author_email='ernestom@mentanetwork.com',
